7bc4d71d977cce8e1ed26540c6d86f772174a133
darlababu60@gmail.com <darlababu60@gmail.com>
2026-02-15 11:49:28 +0000

    c

--- mmodels/kailash.sql
+++ mmodels/kailash.sql
@@ -1,17 +1,10 @@
-{{ config(tags=['naresh','kamaleshwar']) }}
-
+--{{ config(tags=['naresh','kamaleshwar']) }}
 --february 2026
 select * from DB_KAILASH.SCH_KAILASH.EMPLOYEE --country_codes  ##EMPLOYEE
-
 --select c.* from DB_KAILASH.SCH_KAILASH.BKP_EMPLOYEE c
-
 --select * from DB_KAILASH.SCH_KAILASH.EMPLOYEE   --country_codes  ##EMPLOYEE
-
-
 --select c.* from DB_KAILASH.SCH_KAILASH.EMPLOYEE_BKP c kailash,kamalesh,naresh miltry family
-
 --select c.* from DB_KAILASH.SCH_KAILASH.EMPLOYEE_BKP c kailash,kamalesh,naresh,srinivasulu,darla's family
-
 --ENO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DPNO
           
 
