ec489e9de38b60f7db86cb0d59d0c3b3694be217
babu d <darlababu60@gmail.com>
2026-02-18 13:56:00 +0000

    cc

--- models/mdl_incremental.sql
+++ models/mdl_incremental.sql
@@ -1,10 +1,12 @@
 {{config(materialized='incremental') }}     --created as table in test_schema
 
-select *  from {{source("kailash_sources","EMPLOYEE")}}
-
-where hiredate >= {{ var("start_date") }}
+select *  from {{source("kailash_sources","EMPLOYEE")}}  where hiredate >= to_date('{{ var("start_date") }}')
+--where hiredate >= {{ var("start_date") }}-not accepted this
+--where hiredate >= '{{ var("start_date") }}'
+--where hiredate >= to_date('{{ var("start_date") }}')
 
 /*
+
 {% if is_incremental() %}
   where updated_at > (select max(updated_at) from {{ this }})
 {% endif %}
