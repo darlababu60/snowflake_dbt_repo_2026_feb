1637f8df5477ba56476d4a0887fb079817b61d44
babu d <darlababu60@gmail.com>
2026-03-05 04:52:09 +0000

    commit march 5th

--- models/mdl_sample.sql
+++ models/mdl_sample.sql
@@ -2,15 +2,16 @@
 ----column order_date not available so dbt run -s mdl_sample----> fails
 SELECT
   date_trunc('month', HIREDATE) as month,-- to the first day of month--> 2024-03-01 00:00:00
-  SUM(amount) as monthly_sales
+  SUM(sal) as monthly_sales
 FROM {{ ref('kamaleshwar') }}
 WHERE 
-  order_date >= '{{ var("start_date") }}'
-  AND order_date <= '{{ var("end_date") }}'
-  AND  status = '{{ var("user_status", "active") }}'
-  {% if var('row_limit') %}
-  LIMIT {{ var('row_limit') }}
-  {% endif %}
+  HIREDATE >= '{{ var("start_date") }}'
+  AND HIREDATE <= '{{ var("end_date") }}'
+  AND  ENAME = '{{ var("user_status", "active") }}'
+  GROUP BY 1
+{% if var('row_limit', none) %}
+LIMIT {{ var('row_limit') }}
+{% endif %}
   -- dbt test -m mdl_sample-------------------wkd
   --dbt run --vars '{"start_date": "2023-01-01", "end_date": "2023-03-31"}'
   --dbt run --vars '{"regions": ["north", "south"], "include_test_data": false}'
@@ -18,6 +19,6 @@ WHERE
   -- Configuring a model to use environment variables
 {#
 {{ config(schema=env_var('DBT_SCHEMA', 'analytics')) }}
-
 SELECT * FROM {{ ref('stg_orders') }}
 #}
+-- [KAMALESHWAR.HIREDATE] is not a valid group by expression--so group by 1 
--- models/mdl_sample4.sql
+++ models/mdl_sample4.sql
@@ -1,1 +1,10 @@
-models/kailash.sql
+SELECT
+  e.*,
+  CASE
+    WHEN sal >= 1000 THEN 'high'
+    WHEN sal >= 500 THEN 'medium'
+    ELSE 'low'
+  END as spending_segment
+FROM {{ ref('kailashh') }} e
+
+--syntax error line 9 at position 2 unexpected 'CASE'....so ,
