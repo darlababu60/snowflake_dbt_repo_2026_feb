edfa4b04a642387219a26f82902802ace80a35af
babu d <darlababu60@gmail.com>
2026-03-07 17:59:24 +0000

    cmmit  mrch 7-11:29 pm

--- models/kailashh.sql
+++ models/kailashh.sql
@@ -1,5 +1,9 @@
 
-    {{ config(materialized='table',target_lag='1 hour',
+--Query tag -->Q tags are a Snowflake parameter that can be quite useful later on when searching in the QUERY_HISTORY view.
+
+
+    {{ config(materialized='table',target_lag='1 hour',transient=true,query_tag = 'dbt_special',
+    incremental_strategy='merge',
               snowflake_warehouse='compute_wh',tags=["finance", "daily_refresh"],
               
               ) }}  
--- models/md_persons_sample.sql
+++ models/md_persons_sample.sql
@@ -1,10 +1,26 @@
-{{ config(materialized='table') }} 
+{{ config(materialized='incremental',
+        merge_update_columns = ['email', 'ip_address'], incremental_predicates: ["DBT_INTERNAL_DEST.session_start > dateadd(day, -7, current_date)"]
+        merge_exclude_columns = ['email', 'ip_address'],) }} 
 --no config so it is created as view -md_persons_sample in database. test_db,test_schema
 select
 	cast(ENO as string) as emp_no,
 	--order_id, this row is commented out
     cast(ename as string) as emp_name
 from {{ ref ('kailashh') }} 
+
+{% if is_incremental() %}
+        where session_start >= dateadd(day, -3, current_date)
+    {% endif %}
+
+{% if is_incremental() %}
+        where session_start >= dateadd(day, -3, current_date)
+{% endif %}
+
+
+    
 --******** ref will not reffer model available like kailash...it will refer only database table,view,ephemeral only
 --*******model level confi file is higest priority
 --ok done
+--merge_update_columns = ['email', 'ip_address'],...how working
+--merge_exclude_columns = ['email', 'ip_address'],...,...how working
+--incremental_predicates: ["DBT_INTERNAL_DEST.session_start > dateadd(day, -7, current_date)"]....how working?
