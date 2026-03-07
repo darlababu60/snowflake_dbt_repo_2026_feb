ea40381050e985b7a4456a4c0a01ea60c2b4bdcd
babu d <darlababu60@gmail.com>
2026-03-07 04:06:43 +0000

    commit march 7 9:32am

--- models/example/my_1dbtmodel.sql
+++ models/example/my_1dbtmodel.sql
@@ -10,16 +10,11 @@
 {{ config(materialized='table') }}
 
 with source_data as (
-
     select 1 as id
     union all
-    select null as id
-
-)
-
+    select null as id)
 select *
 from source_data
-
 /*
     Uncomment the line below to remove records with null `id` values
 */
